93de4a23b43dd10e9a1c5d63a2716fbecb874344
Jim Cromie <jim.cromie@gmail.com>
2021-03-16 23:24:47 -0600

    dyndbg: RFC handle __dyndbg* sections in module.lds.h
    For a long time now, loadable modules have tacitly linked the __dyndbg
    section into the .ko, as is observable in dmesg by enabling module.c's
    pr_debugs and loading a module.  Recently __dyndbg_sites was added,
    following the original model.
    
    But now, we need to explicitly name those (__dyndbg, __dyndbg_sites)
    sections in order to place new .gnu.linkonce.dyndbg* sections in front
    of them.
    
    This gives us the properties we need for _ddebug_map_site() to drop
    the _ddebug.site pointer:
    
       fixed offset from &__dyndbg[N] to &__dyndbg[0]
       container_of gets &header
       header has ptr to __dyndbg_sites[]
       __dyndbg_sites[_map] gives de-duplicated site recs
    
    TODO ?
    
    At this point, for loaded modules, ddebug_add_module() sees the header
    as 0'th element.  This is harmless; theres no callsite to invoke on
    the descriptor.  That said, this may yet need adjustments:
    
    - subtract 1 from callsite count before creating "empty" ddebug-tables
      records.  This is mostly minor optimization.
    
    - offset 1 - maybe, not observed.
    
    Signed-off-by: Jim Cromie <jim.cromie@gmail.com>

--- include/asm-generic/module.lds.h
+++ include/asm-generic/module.lds.h
@@ -4,7 +4,17 @@
 
 /*
  * <asm/module.lds.h> can specify arch-specific sections for linking modules.
- * Empty for the asm-generic header.
+ *
+ * For loadable modules with CONFIG_DYNAMIC_DEBUG, we need to keep the
+ * 2 __dyndbg* ELF sections, which are loaded by module.c
+ *
+ * Pack the 2 __dyndbg* input sections with their respective
+ * .gnu.linkonce. header records into 2 output sections, with those
+ * header records in the 0th element.
  */
+SECTIONS {
+__dyndbg_sites	: ALIGN(8) { *(.gnu.linkonce.dyndbg_site) *(__dyndbg_sites) }
+__dyndbg	: ALIGN(8) { *(.gnu.linkonce.dyndbg)	  *(__dyndbg) }
+}
 
 #endif /* __ASM_GENERIC_MODULE_LDS_H */
