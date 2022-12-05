d60530ace2b2ef88be41858b36a38c90777d58ff
Jim Cromie <jim.cromie@gmail.com>
2022-12-04 17:59:10 -0700

    dyndbg: reduce verbose/debug clutter
    currently, for verbose=3, these are logged (blank lines for clarity):
    
     dyndbg: query 0: "class DRM_UT_CORE +p" mod:*
     dyndbg: split into words: "class" "DRM_UT_CORE" "+p"
    
     dyndbg: op='+'
     dyndbg: flags=0x1
     dyndbg: *flagsp=0x1 *maskp=0xffffffff
    
     dyndbg: parsed: func="" file="" module="" format="" lineno=0-0 class=...
     dyndbg: no matches for query
     dyndbg: no-match: func="" file="" module="" format="" lineno=0-0 class=...
     dyndbg: processed 1 queries, with 0 matches, 0 errs
    
    That is excessive, so this patch:
     - shrinks 3 lines of 2nd stanza to single line
     - drops 1st 2 lines of 3rd stanza
       3rd line is like 1st, with result, not procedure.
       2nd line is just status, retold in 4th, with more info.
    
    New output:
    
     dyndbg: query 0: "class DRM_UT_CORE +p" mod:*
     dyndbg: split into words: "class" "DRM_UT_CORE" "+p"
     dyndbg: op='+' flags=0x1 *flagsp=0x1 *maskp=0xffffffff
     dyndbg: no-match: func="" file="" module="" format="" lineno=0-0 class=...
     dyndbg: processed 1 queries, with 0 matches, 0 errs
    
    no functional change.
    
    Signed-off-by: Jim Cromie <jim.cromie@gmail.com>

--- lib/dynamic_debug.c
+++ lib/dynamic_debug.c
@@ -281,9 +281,6 @@ static int ddebug_change(const struct ddebug_query *query,
 	}
 	mutex_unlock(&ddebug_lock);
 
-	if (!nfound && verbose)
-		pr_info("no matches for query\n");
-
 	return nfound;
 }
 
@@ -516,7 +513,6 @@ static int ddebug_parse_flags(const char *str, struct flag_settings *modifiers)
 		pr_err("bad flag-op %c, at start of %s\n", *str, str);
 		return -EINVAL;
 	}
-	v3pr_info("op='%c'\n", op);
 
 	for (; *str ; ++str) {
 		for (i = ARRAY_SIZE(opt_array) - 1; i >= 0; i--) {
@@ -530,7 +526,6 @@ static int ddebug_parse_flags(const char *str, struct flag_settings *modifiers)
 			return -EINVAL;
 		}
 	}
-	v3pr_info("flags=0x%x\n", modifiers->flags);
 
 	/* calculate final flags, mask based upon op */
 	switch (op) {
@@ -546,7 +541,7 @@ static int ddebug_parse_flags(const char *str, struct flag_settings *modifiers)
 		modifiers->flags = 0;
 		break;
 	}
-	v3pr_info("*flagsp=0x%x *maskp=0x%x\n", modifiers->flags, modifiers->mask);
+	v3pr_info("op='%c' flags=0x%x maskp=0x%x\n", op, modifiers->flags, modifiers->mask);
 
 	return 0;
 }
@@ -556,7 +551,7 @@ static int ddebug_exec_query(char *query_string, const char *modname)
 	struct flag_settings modifiers = {};
 	struct ddebug_query query = {};
 #define MAXWORDS 9
-	int nwords, nfound;
+	int nwords;
 	char *words[MAXWORDS];
 
 	nwords = ddebug_tokenize(query_string, words, MAXWORDS);
@@ -574,10 +569,7 @@ static int ddebug_exec_query(char *query_string, const char *modname)
 		return -EINVAL;
 	}
 	/* actually go and implement the change */
-	nfound = ddebug_change(&query, &modifiers);
-	vpr_info_dq(&query, nfound ? "applied" : "no-match");
-
-	return nfound;
+	return ddebug_change(&query, &modifiers);
 }
 
 /* handle multiple queries in query string, continue on error, return
