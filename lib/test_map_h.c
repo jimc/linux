eb67307aad42760b3e9886301d3fb33614651724
Jim Cromie <jim.cromie@gmail.com>
2023-03-25 09:01:51 -0600

    lib/test_map_h: add new module to test map.h macros
    Build test_map_h.ko to validate MAP & MAP_LIST
    
    To test include/linux/map.h, this adds test_map_h.ko.  The code uses
    BUILD_BUG_ON, so that failures of the macro just break the build.
    
    This adds config CONFIG_TEST_MAP_H stanza into:
    menu "Compile-time checks and compiler options",
    and fixes endmenu comment to match.
    
    The BUILD_BUG_ON construct avoids __i386__, maybe temporary, cleaned
    later for lkp isolation of cause.  Alt cause is calling
    MAPH_BUILD_BUG, dropped now.
    
    Module does runtime tests too, including a failure and warn.
    
    bash-5.2# modprobe test_map_h
    [37051.412761] dyndbg: add-module: test_map_h.6 sites
    [37051.413443] dyndbg:   6 debug prints in module test_map_h
    [37051.415210] failed: DRMx_DRIVER eq 'not-in-map'
    
    default m, to maximize robot coverage.
    
    Signed-off-by: Jim Cromie <jim.cromie@gmail.com>
    
    dont-call-MAPH_BUILD_BUG

---
+++ lib/test_map_h.c
@@ -0,0 +1,85 @@
+// SPDX-License-Identifier: GPL-2.0-only
+/*
+ * test include/linux/map.h macros: MAP_LIST & MAP
+ *
+ * Authors:
+ *      Jim Cromie	<jim.cromie@gmail.com>
+ */
+
+#include <linux/map.h>
+#include <linux/module.h>
+
+/* compile-time test MAP */
+static int sum = MAP( +, 1, 2, 3);
+static char* caten = MAP(__stringify, YES, NO, MAYBE);
+
+/* client/user defines enum NAMES as source of truth */
+enum client_categories { DRMx_CORE, DRMx_DRIVER, DRMx_KMS };
+
+/* test MAP_LIST(__stringify, ...). others deemed equivalent */
+#define STRVEC_FROM_ENUM_VALS_(_vec_, ...)			\
+	const char *_vec_##_names[] = {					\
+		MAP_LIST(__stringify, ##__VA_ARGS__) }
+
+static STRVEC_FROM_ENUM_VALS_(debug_cats, DRMx_CORE, DRMx_DRIVER, DRMx_KMS);
+
+#if !defined(__i386__)
+#define BUILD_BUG_STREQ(_var, ref)					\
+	BUILD_BUG_ON(__builtin_memcmp(_var, ref, __builtin_strlen(ref)))
+#else
+#define BUILD_BUG_STREQ(_var, ref)	 BUILD_BUG_ON(0)
+#endif
+#define BUILD_BUG_STREQ_VI(_vec_, idx, ref)		\
+	BUILD_BUG_STREQ(_vec_##_names[idx], ref)
+
+/* force compile, even if its not called */
+static void __used MAPH_BUILD_BUG(void)
+{
+	BUILD_BUG_ON(sum != 6);
+	BUILD_BUG_STREQ(caten, "YESNOMAYBE");
+
+	BUILD_BUG_STREQ_VI(debug_cats, 0, "DRMx_CORE");
+	BUILD_BUG_STREQ_VI(debug_cats, 1, "DRMx_DRIVER");
+	BUILD_BUG_STREQ_VI(debug_cats, 2, "DRMx_KMS");
+
+#ifdef MAPH_FORCE_FAIL
+	/* either breaks compile */
+	BUILD_BUG_ON(sum != 8);
+	BUILD_BUG_STREQ(debug_cats, 1, "'not-in-map'");
+#endif
+}
+
+#define rTEST_eq(_v_, _ref)						\
+	({								\
+		if (strcmp(_v_, _ref)) {				\
+			pr_warn("failed: %s eq %s\n", _v_, _ref);	\
+		} else {						\
+			pr_debug("ok: %s eq %s\n", _v_, _ref);		\
+		}							\
+	})
+#define rTEST_eq_VI(_v_, _i, _ref)	rTEST_eq(_v_##_names[_i], _ref)
+
+static int __init test_maph_init(void)
+{
+	pr_debug("init start\n");
+
+	rTEST_eq(caten, "YESNOMAYBE");
+	rTEST_eq_VI(debug_cats, 0, "DRMx_CORE");
+	rTEST_eq_VI(debug_cats, 1, "DRMx_DRIVER");
+	rTEST_eq_VI(debug_cats, 2, "DRMx_KMS");
+
+	/* force fail and ignore */
+	rTEST_eq_VI(debug_cats, 1, "'not-in-map'");
+	return 0;
+}
+
+static void __exit test_maph_exit(void)
+{
+	pr_debug("exited\n");
+}
+
+module_init(test_maph_init);
+module_exit(test_maph_exit);
+
+MODULE_AUTHOR("Jim Cromie <jim.cromie@gmail.com>");
+MODULE_LICENSE("GPL");
