a8e46493c7199c2e95dc4eae9d0d9275c963561e
Jim Cromie <jim.cromie@gmail.com>
2023-09-28 00:41:34 -0600

    dyndbg: drop _ddebug.site member
    drop the _ddebug.site ptr, and rely upon the maple-tree intervals
    loaded by ddebug_condense_sites().
    
    In DEFINE_DYNAMIC_DEBUG_METADATA_CLS(), which creates a pair of
    _ddebug & _ddebug_site structs, I had to add '__used' to the latter,
    because it is no longer linked from the former, and is omitted during
    linkage.  And then also to the former, so they both have the same
    number of entries.
    
    Without the ptr from desc to site, the (num_descs == num_sites)
    condition is the only way to know that the 2 vectors of 1/2 records:
    (desc,site) are properly paired up with each other.  These invariants
    are enforced by the BUG_ONs added in an earlier commit, and are what
    led to the addition of '__used'.
    
    NOTE: with the '__used' above, ~20 extra records (of ~3500 builtins)
    are linked.  I haven't dug for a cause.
    
    RFC: is there any "symbolic" or __virtual linkage possible?
    A way to link 2 records w/o needing a pointer between them, such that
    the linker will treat them as both used if either one is ?
    
    Signed-off-by: Jim Cromie <jim.cromie@gmail.com>

--- lib/dynamic_debug.c
+++ lib/dynamic_debug.c
@@ -1712,8 +1712,6 @@ static int __init dynamic_debug_init(void)
 
 	for (; iter < __stop___dyndbg; iter++, site++, i++, mod_sites++) {
 
-		BUG_ON(site != iter->site);
-
 		if (strcmp(modname, site_modname(site))) {
 			mod_ct++;
 			di.descs.start = iter_mod_start;
