42466e655730ace8b0e24874d3b08ef9dc44500d
Jim Cromie <jim.cromie@gmail.com>
2023-10-02 16:12:48 -0600

    dyndbg: cache the dynamically generated prefixes per callsite
    Currently __dynamic_emit_lookup() re-writes a pr_debug's dynamic
    prefix each time it is invoked.  For a busy pr_debug with +mfslt
    prefix-flags, this can get expensive.
    
    And with accessors replacing direct field refs with maple-tree
    lookups, the dynamic-prefix costs likely increase.
    
    Add a maple-tree to cache these prefixes per-callsite, as they're
    created, and reuse them once they already exist.  Delete entries
    anytime the flags are altered, and let them be recreated as needed.
    
    This should result in a net decrease in the cost of using prefix
    flags.  TODO: figure out how to measure this.
    
    NOTES
    
    ddebug descriptor loses the const so the _CACHED flag can be set.
    
    The prefix stringbuf is still allocated on the stack, but newly copied
    to kmalloc'd space fitted to the actual string.  TODO: Since code
    already has 128 fixed buffer size, just pre-allocate that.
    
    ATM, the prefix is stored on the site descriptor, ignoring any
    possible sharing as too complicated to grasp now.
    
    If a single maple tree can contain all 3 nested intervals, and the
    tree-walk to a descriptor could optionally emit the internal "pivot?"
    values (the modname, filename, function) without much added cost, then
    maybe the cache is only marginally faster (since once cached, its
    found by traversal w/o the pivots).
    
    Signed-off-by: Jim Cromie <jim.cromie@gmail.com>

--- lib/dynamic_debug.c
+++ lib/dynamic_debug.c
@@ -114,6 +114,14 @@ static const struct kernel_param_ops param_ops_do_scan = {
 };
 module_param_cb(do_scan, &param_ops_do_scan, NULL, 0600);
 
+/* cache of composed prefixes for enabled pr_debugs */
+static DEFINE_MTREE(pr_prefixes);
+
+static void ddebug_clear_prefix_cache(const struct _ddebug *dp)
+{
+	mtree_erase(&pr_prefixes, (unsigned long)dp);
+}
+
 /* Return the path relative to source root */
 static inline const char *trim_prefix(const char *path)
 {
@@ -336,6 +344,7 @@ static int ddebug_change(const struct ddebug_query *query, struct flag_settings 
 			newflags = (dp->flags & modifiers->mask) | modifiers->flags;
 			if (newflags == dp->flags)
 				continue;
+			ddebug_clear_prefix_cache(dp);
 #ifdef CONFIG_JUMP_LABEL
 			if (dp->flags & _DPRINTK_FLAGS_PRINT) {
 				if (!(newflags & _DPRINTK_FLAGS_PRINT))
@@ -854,8 +863,18 @@ static int remaining(int wrote)
 	return 0;
 }
 
-static int __dynamic_emit_lookup(const struct _ddebug *desc, char *buf, int pos)
+static int __dynamic_emit_lookup(struct _ddebug *desc, char *buf, int pos)
 {
+	char *prefix, *cpy;
+
+	if (desc->flags & _DPRINTK_FLAGS_PREFIX_CACHED) {
+		prefix = (char *) mtree_load(&pr_prefixes, (unsigned long)desc);
+		if (prefix) {
+			pos += snprintf(buf + pos, remaining(pos), "%s", prefix);
+			v4pr_info("using prefix cache:%px %s", buf, buf + pos);
+			return pos;
+		}
+	}
 	if (!(desc->flags & _DPRINTK_FLAGS_INCL_LOOKUP))
 		return pos;
 
@@ -871,6 +890,12 @@ static int __dynamic_emit_lookup(const struct _ddebug *desc, char *buf, int pos)
 	if (desc->flags & _DPRINTK_FLAGS_INCL_LINENO)
 		pos += snprintf(buf + pos, remaining(pos), "%d:",
 				desc->lineno);
+
+	/* save dup of buf to cache */
+	cpy = kstrdup(buf + pos, GFP_KERNEL);
+	mtree_store(&pr_prefixes, (unsigned long)desc, (void *)cpy, GFP_KERNEL);
+	desc->flags |= _DPRINTK_FLAGS_PREFIX_CACHED;
+	v3pr_info("filling prefix cache:%px %s", desc, cpy);
 
 	return pos;
 }
