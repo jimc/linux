200a8519206d2c6a12bf4754235cf772e8283c60
Jim Cromie <jim.cromie@gmail.com>
2023-12-01 00:40:34 +0100

    dyndbg: add _DPRINTK_FLAGS_ENABLED
    Distinguish the condition: _DPRINTK_FLAGS_ENABLED from the bit:
    _DPRINTK_FLAGS_PRINT, and re-define former in terms of latter, in
    preparation to add a 2nd bit: _DPRINTK_FLAGS_TRACE
    
    Update JUMP_LABEL code block to check _DPRINTK_FLAGS_ENABLED symbol.
    Also add a 'K' to get new symbol _DPRINTK_FLAGS_PRINTK, in order to
    break any stale uses.
    
    CC: vincent.whitchurch@axis.com
    Signed-off-by: Jim Cromie <jim.cromie@gmail.com>

--- lib/dynamic_debug.c
+++ lib/dynamic_debug.c
@@ -89,7 +89,7 @@ static inline const char *trim_prefix(const char *path)
 }
 
 static const struct { unsigned flag:8; char opt_char; } opt_array[] = {
-	{ _DPRINTK_FLAGS_PRINT, 'p' },
+	{ _DPRINTK_FLAGS_PRINTK, 'p' },
 	{ _DPRINTK_FLAGS_INCL_MODNAME, 'm' },
 	{ _DPRINTK_FLAGS_INCL_FUNCNAME, 'f' },
 	{ _DPRINTK_FLAGS_INCL_SOURCENAME, 's' },
@@ -247,10 +247,10 @@ static int ddebug_change(const struct ddebug_query *query,
 			if (newflags == dp->flags)
 				continue;
 #ifdef CONFIG_JUMP_LABEL
-			if (dp->flags & _DPRINTK_FLAGS_PRINT) {
-				if (!(newflags & _DPRINTK_FLAGS_PRINT))
+			if (dp->flags & _DPRINTK_FLAGS_ENABLED) {
+				if (!(newflags & _DPRINTK_FLAGS_ENABLED))
 					static_branch_disable(&dp->key.dd_key_true);
-			} else if (newflags & _DPRINTK_FLAGS_PRINT) {
+			} else if (newflags & _DPRINTK_FLAGS_ENABLED) {
 				static_branch_enable(&dp->key.dd_key_true);
 			}
 #endif
