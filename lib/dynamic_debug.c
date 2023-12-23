8f9f35330251b49e3da243bbe14c623e28c340bd
Jim Cromie <jim.cromie@gmail.com>
2023-12-23 02:51:30 +0100

    dyndbg: reduce verbose/debug clutter
    currently, for verbose=3, these are logged for each query
    
     dyndbg: query 0: <class DRM_UT_CORE +p> mod: <*>
     dyndbg: split into words: "class" "DRM_UT_CORE" "+p"
     dyndbg: op='+'
     dyndbg: flags=0x1
     dyndbg: *flagsp=0x1 *maskp=0xffffffff
     dyndbg: parsed: func="" file="" module="" format="" lineno=0-0 class=...
     dyndbg: no matches for query
     dyndbg: no-match: func="" file="" module="" format="" lineno=0-0 class=...
     dyndbg: processed 1 queries, with 0 matches, 0 errs
    
    That is excessive, so this patch shrinks it to 4 lines:
    
     dyndbg: query 0: <class D2_CORE +T:foo > on module: <*>
     dyndbg: split into words: "class" "D2_CORE" "+T:foo"
     dyndbg: op='+' flags=0x40 maskp=0xffffffff trace_dest=0x0
     dyndbg: applied: func="" file="" module="" format="" lineno=0-0 class=...
    
    Signed-off-by: Jim Cromie <jim.cromie@gmail.com>

--- lib/dynamic_debug.c
+++ lib/dynamic_debug.c
@@ -835,7 +835,6 @@ static int ddebug_parse_query(char *words[], int nwords,
 		 */
 		query->module = modname;
 
-	vpr_info_dq(query, "parsed");
 	return 0;
 }
 
@@ -860,7 +859,6 @@ static int ddebug_parse_flags(const char *str, struct flag_settings *modifiers)
 		pr_err("bad flag-op %c, at start of %s\n", *str, str);
 		return -EINVAL;
 	}
-	v3pr_info("op='%c'\n", op);
 
 	for (; *str ; ++str) {
 		for (i = ARRAY_SIZE(opt_array) - 1; i >= 0; i--) {
@@ -880,7 +878,6 @@ static int ddebug_parse_flags(const char *str, struct flag_settings *modifiers)
 			return -EINVAL;
 		}
 	}
-	v3pr_info("flags=0x%x, trace_dest=0x%x\n", modifiers->flags, modifiers->trace_dst);
 
 	/* calculate final flags, mask based upon op */
 	switch (op) {
@@ -896,7 +893,8 @@ static int ddebug_parse_flags(const char *str, struct flag_settings *modifiers)
 		modifiers->flags = 0;
 		break;
 	}
-	v3pr_info("*flagsp=0x%x *maskp=0x%x\n", modifiers->flags, modifiers->mask);
+	v3pr_info("op='%c' flags=0x%x maskp=0x%x trace_dest=0x%x\n",
+		  op, modifiers->flags, modifiers->mask, modifiers->trace_dst);
 
 	return 0;
 }
