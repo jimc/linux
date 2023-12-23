d9b2341b2afe44721eec56abdd68c57efe7e411c
Jim Cromie <jim.cromie@gmail.com>
2023-12-23 02:51:27 +0100

    dyndbg: treat comma as a token separator
    Treat comma as a token terminator, just like a space.  This allows a
    user to avoid quoting hassles when spaces are otherwise needed:
    
     :#> modprobe drm dyndbg=class,DRM_UT_CORE,+p\;class,DRM_UT_KMS,+p
    
    or as a boot arg:
    
      drm.dyndbg=class,DRM_UT_CORE,+p  # todo: support multi-query here
    
    Given the myriad ways a boot-line can be assembled and then passed
    in/down/around shell based tools, if the >control parser treats commas
    like spacees, this would allow side-stepping all sorts of quoting
    hassles thru those layers.
    
    Signed-off-by: Jim Cromie <jim.cromie@gmail.com>
    Signed-off-by: Łukasz Bartosik <lb@semihalf.com>
    ---
    - squash helper-fn mod skip_spaces_and_comma()

--- lib/dynamic_debug.c
+++ lib/dynamic_debug.c
@@ -618,6 +618,14 @@ static int ddebug_change(const struct ddebug_query *query,
 	return nfound;
 }
 
+char *skip_spaces_and_coma(const char *str)
+{
+	str = skip_spaces(str);
+	if (*str == ',')
+		str = skip_spaces(++str);
+	return (char *)str;
+}
+
 /*
  * Split the buffer `buf' into space-separated words.
  * Handles simple " and ' quoting, i.e. without nested,
@@ -631,12 +639,16 @@ static int ddebug_tokenize(char *buf, char *words[], int maxwords)
 	while (*buf) {
 		char *end;
 
-		/* Skip leading whitespace */
-		buf = skip_spaces(buf);
+		/* Skip leading whitespace and coma */
+		buf = skip_spaces_and_coma(buf);
 		if (!*buf)
 			break;	/* oh, it was trailing whitespace */
 		if (*buf == '#')
 			break;	/* token starts comment, skip rest of line */
+		if (*buf == ',') {
+			buf++;
+			continue;
+		}
 
 		/* find `end' of word, whitespace separated or quoted */
 		if (*buf == '"' || *buf == '\'') {
@@ -648,7 +660,7 @@ static int ddebug_tokenize(char *buf, char *words[], int maxwords)
 				return -EINVAL;	/* unclosed quote */
 			}
 		} else {
-			for (end = buf; *end && !isspace(*end); end++)
+			for (end = buf; *end && !isspace(*end) && *end != ','; end++)
 				;
 			if (end == buf) {
 				pr_err("parse err after word:%d=%s\n", nwords,
@@ -955,7 +967,7 @@ static int ddebug_exec_queries(char *query, const char *modname)
 		if (split)
 			*split++ = '\0';
 
-		query = skip_spaces(query);
+		query = skip_spaces_and_coma(query);
 		if (!query || !*query || *query == '#')
 			continue;
 
