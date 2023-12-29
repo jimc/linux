345145b7be8c1a1746505281edd2db0b9b4f408e
Jim Cromie <jim.cromie@gmail.com>
2023-12-28 23:39:16 -0700

    dyndbg-test: fixup search-trace-name help
    The recent change to do whole-buf search (not just single line) had
    the side-effect of printing too much $output, which hides the
    information in clutter.  Print the search pattern instead.
    
    Also add -v last arg handling, like in check_match_ct().  This lets a
    single caller enable verbose output, to see the trace-bufs content.
    
    Signed-off-by: Jim Cromie <jim.cromie@gmail.com>

--- tools/testing/selftests/dynamic_debug/dyndbg_selftest.sh
+++ tools/testing/selftests/dynamic_debug/dyndbg_selftest.sh
@@ -143,6 +143,7 @@ function search_trace() {
 # $1 - trace instance name, 0 for global event trace
 # $2 - line number counting from the bottom
 # $3 - text to search for
+# $4 - optional -v to see verbose results
 function search_trace_name() {
 	if [ "$1" = "0" ]; then
 	    buf=$(cat /sys/kernel/debug/tracing/trace)
@@ -161,8 +162,8 @@ function search_trace_name() {
             echo -e "${RED}: $BASH_SOURCE:$BASH_LINENO search for '$3' failed in line '$line' or '$buf'"
 	    exit
 	fi
-	if [ $v_search_trace = 1 ]; then
-	    echo -e "${MAGENTA}: search_trace_name in $1 found: \n$output \nin:${BLUE} $buf ${NC}"
+	if [[ "$4" == "-v" || "$v_search_trace" = 1 ]]; then
+	    echo -e "${MAGENTA}: search_trace_name in $1 found: \n${YELLOW}$3 \nin:${BLUE} $buf ${NC}"
         fi
 }
 
