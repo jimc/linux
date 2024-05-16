f097a7e5cef96a434efe047cb4a267b80b376934
Jim Cromie <jim.cromie@gmail.com>
2024-05-15 22:49:36 -0600

    checkpatch: allow DYNDBG_CLASSMAP_* macros past do-while advice
    Checkpatch mostly disallows multi-statement macros, advising to wrap
    them in a do-while.  It allows a few exceptions; obvious storage
    declarations, and several macros by name.
    
    Add a new exception for DYNDBG_CLASSMAP_* macros with multiple statements:
    
    DYNDBG_CLASSMAP_DEFINE: 1st statement defines storage, 2nd EXPORTs it.
    DYNDBG_CLASSMAP_PARAM*: works like module_param_named
    
    cc: Andy Whitcroft <apw@canonical.com>		# (maintainer:CHECKPATCH)
    cc: Joe Perches <joe@perches.com>		# (maintainer:CHECKPATCH)
    cc: Dwaipayan Ray <dwaipayanray1@gmail.com>	# (reviewer:CHECKPATCH)
    cc: Lukas Bulwahn <lukas.bulwahn@gmail.com>	# (reviewer:CHECKPATCH)
    Signed-off-by: Jim Cromie <jim.cromie@gmail.com>
    ---
    The DYNDBG_CLASSMAP_* macros are submitted for 6.10

--- scripts/checkpatch.pl
+++ scripts/checkpatch.pl
@@ -5864,6 +5864,7 @@ sub process {
 			}
 		}
 
+# except for declarative macros (whether file or function scope),
 # multi-statement macros should be enclosed in a do while loop, grab the
 # first statement and ensure its the whole macro if its not enclosed
 # in a known good container
@@ -5923,6 +5924,7 @@ sub process {
 				MODULE_PARM_DESC|
 				DECLARE_PER_CPU|
 				DEFINE_PER_CPU|
+				(?:__)?DYNDBG_CLASSMAP_\w+(?:_\w+)?|
 				__typeof__\(|
 				union|
 				struct|
