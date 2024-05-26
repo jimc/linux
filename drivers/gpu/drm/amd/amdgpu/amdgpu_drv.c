c1ee5041101dda7ddcd66da8aca93a9714dd2d98
Jim Cromie <jim.cromie@gmail.com>
2024-05-25 22:00:39 -0600

    drm-dyndbg: DRM_CLASSMAP_USE in amdgpu driver
    Following the dyndbg-api-fix, replace DECLARE_DYNDBG_CLASSMAP with
    DRM_CLASSMAP_USE.  This refs the defined & exported classmap, rather
    than re-declaring it redundantly, and error-prone-ly.
    
    This resolves the appearance of "class:_UNKNOWN_" in the control file
    for the driver's drm_dbg()s.
    
    Fixes: f158936b60a7 ("drm: POC drm on dyndbg - use in core, 2 helpers, 3 drivers.")
    
    Signed-off-by: Jim Cromie <jim.cromie@gmail.com>
    Reviewed-by: Louis Chauvet <louis.chauvet@bootlin.com>

--- drivers/gpu/drm/amd/amdgpu/amdgpu_drv.c
+++ drivers/gpu/drm/amd/amdgpu/amdgpu_drv.c
@@ -245,17 +245,7 @@ int amdgpu_umsch_mm_fwlog;
 int amdgpu_rebar = -1; /* auto */
 int amdgpu_user_queue = -1;
 
-DECLARE_DYNDBG_CLASSMAP(drm_debug_classes, DD_CLASS_TYPE_DISJOINT_BITS, 0,
-			"DRM_UT_CORE",
-			"DRM_UT_DRIVER",
-			"DRM_UT_KMS",
-			"DRM_UT_PRIME",
-			"DRM_UT_ATOMIC",
-			"DRM_UT_VBL",
-			"DRM_UT_STATE",
-			"DRM_UT_LEASE",
-			"DRM_UT_DP",
-			"DRM_UT_DRMRES");
+DRM_CLASSMAP_USE(drm_debug_classes);
 
 struct amdgpu_mgpu_info mgpu_info = {
 	.mutex = __MUTEX_INITIALIZER(mgpu_info.mutex),
