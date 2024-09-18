2f1ac1b8daf4a75c10debbc208f5a7379fbc1f9c
Jim Cromie <jim.cromie@gmail.com>
2024-09-18 17:35:50 -0600

    dyndbg: check DYNAMIC_DEBUG_CLASSMAP_DEFINE args at compile-time
    Add __DYNAMIC_DEBUG_CLASSMAP_CHECK to implement the following
    arg-checks at compile-time:
    
    	0 <= _base < 63
    	class_names is not empty
    	class_names[0] is a string
    	(class_names.length + _base) < 63
    
    These compile-time checks will prevent several misuses; 4 such
    examples are added to test_dynamic_debug_submod.ko, and will fail
    compilation if -DDD_MACRO_ARGCHECK is added to cflags.  This wouldn't
    be a useful CONFIG_ item, since it breaks the build.
    
    Signed-off-by: Jim Cromie <jim.cromie@gmail.com>
    ---
    v3- $macro_name =~ s/DYNDBG/DYNAMIC_DEBUG/
    
    prev-
    - split static-asserts to __DYNDBG_CLASSMAP_CHECK
    - move __DYNDBG_CLASSMAP_CHECK above kdoc for DYNDBG_CLASSMAP_DEFINE
      silences kernel-doc warnings

--- lib/test_dynamic_debug.c
+++ lib/test_dynamic_debug.c
@@ -146,7 +146,18 @@ DYNDBG_CLASSMAP_DEFINE(classid_range_conflict, 0, D2_CORE + 1, "D3_CORE");
 DYNAMIC_DEBUG_CLASSMAP_USE(map_disjoint_bits);
 DYNAMIC_DEBUG_CLASSMAP_USE(map_level_num);
 
+#if defined(DD_MACRO_ARGCHECK)
+/*
+ * Exersize compile-time arg-checks in DYNDBG_CLASSMAP_DEFINE.
+ * These will break compilation.
+ */
+DYNDBG_CLASSMAP_DEFINE(fail_base_neg, 0, -1, "NEGATIVE_BASE_ARG");
+DYNDBG_CLASSMAP_DEFINE(fail_base_big, 0, 100, "TOOBIG_BASE_ARG");
+DYNDBG_CLASSMAP_DEFINE(fail_str_type, 0, 0, 1 /* not a string */);
+DYNDBG_CLASSMAP_DEFINE(fail_emptyclass, 0, 0 /* ,empty */);
 #endif
+
+#endif /* TEST_DYNAMIC_DEBUG_SUBMOD */
 
 /* stand-in for all pr_debug etc */
 #define prdbg(SYM) __pr_debug_cls(SYM, #SYM " msg\n")
