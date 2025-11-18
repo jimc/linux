3ddc9388f1b3af88ba0610b4d1c643a44473cc55
Jim Cromie <jim.cromie@gmail.com>
2025-11-18 13:18:20 -0700

    dyndbg: reduce verbose/debug clutter
    currently, for verbose=3, these are logged (blank lines for clarity):
    
     dyndbg: query 0: "class DRM_UT_CORE +p" mod:*
     dyndbg: split into words: "class" "DRM_UT_CORE" "+p"
    
     dyndbg: op='+'
     dyndbg: flags=0x1
     dyndbg: *flagsp=0x1 *maskp=0xffffffff
    
     dyndbg: parsed: func="" file="" module="" format="" lineno=0-0 class=...
     dyndbg: no matches for query
     dyndbg: no-match: func="" file="" module="" format="" lineno=0-0 class=...
     dyndbg: processed 1 queries, with 0 matches, 0 errs
    
    That is excessive, so this patch:
     - shrinks 3 lines of 2nd stanza to single line
     - drops 1st 2 lines of 3rd stanza
       3rd line is like 1st, with result, not procedure.
       2nd line is just status, retold in 4th, with more info.
    
    New output:
    
     dyndbg: query 0: "class DRM_UT_CORE +p"
     dyndbg: split into words: "class" "DRM_UT_CORE" "+p"
     dyndbg: op='+' flags=0x1 maskp=0xffffffff
     dyndbg: processed 1 queries, with 0 matches, 0 errs
    
    Also drop several verbose=3 messages in ddebug_add_module
    
    When modprobing a module, dyndbg currently logs/says "add-module", and
    then "skipping" if the module has no prdbgs.  Instead just check 1st
    and return quietly.
    
    no functional change
    
    Signed-off-by: Jim Cromie <jim.cromie@gmail.com>
    Reviewed-by: Louis Chauvet <louis.chauvet@bootlin.com>
    ---
    v2:
    
    RvB after SoB
    
    trivial change to verbose-debug output line to output the actual
    "module" keyword rather than "mod:", and do so only when the module is
    constrained by the callchain (ie as part of a modprobe).
    
     was:   query X: "(keyword value)* [+-=]flags" mod:*
     now:   query X: "(keyword value)* [+-=]flags"
       or   query X: module FOO "keyword value)* [+-=]flags"
    
    IOW, adjust output to reflect the input grammar more closely.

--- lib/dynamic_debug.c
+++ lib/dynamic_debug.c
@@ -277,9 +277,6 @@ static int ddebug_change(const struct ddebug_query *query,
 	}
 	mutex_unlock(&ddebug_lock);
 
-	if (!nfound && verbose)
-		pr_info("no matches for query\n");
-
 	return nfound;
 }
 
@@ -512,7 +509,6 @@ static int ddebug_parse_flags(const char *str, struct flag_settings *modifiers)
 		pr_err("bad flag-op %c, at start of %s\n", *str, str);
 		return -EINVAL;
 	}
-	v3pr_info("op='%c'\n", op);
 
 	for (; *str ; ++str) {
 		for (i = ARRAY_SIZE(opt_array) - 1; i >= 0; i--) {
@@ -526,7 +522,6 @@ static int ddebug_parse_flags(const char *str, struct flag_settings *modifiers)
 			return -EINVAL;
 		}
 	}
-	v3pr_info("flags=0x%x\n", modifiers->flags);
 
 	/* calculate final flags, mask based upon op */
 	switch (op) {
@@ -542,7 +537,7 @@ static int ddebug_parse_flags(const char *str, struct flag_settings *modifiers)
 		modifiers->flags = 0;
 		break;
 	}
-	v3pr_info("*flagsp=0x%x *maskp=0x%x\n", modifiers->flags, modifiers->mask);
+	v3pr_info("op='%c' flags=0x%x maskp=0x%x\n", op, modifiers->flags, modifiers->mask);
 
 	return 0;
 }
@@ -552,7 +547,7 @@ static int ddebug_exec_query(char *query_string, const char *modname)
 	struct flag_settings modifiers = {};
 	struct ddebug_query query = {};
 #define MAXWORDS 9
-	int nwords, nfound;
+	int nwords;
 	char *words[MAXWORDS];
 
 	nwords = ddebug_tokenize(query_string, words, MAXWORDS);
@@ -570,10 +565,7 @@ static int ddebug_exec_query(char *query_string, const char *modname)
 		return -EINVAL;
 	}
 	/* actually go and implement the change */
-	nfound = ddebug_change(&query, &modifiers);
-	vpr_info_dq(&query, nfound ? "applied" : "no-match");
-
-	return nfound;
+	return ddebug_change(&query, &modifiers);
 }
 
 /* handle multiple queries in query string, continue on error, return
@@ -594,7 +586,8 @@ static int ddebug_exec_queries(char *query, const char *modname)
 		if (!query || !*query || *query == '#')
 			continue;
 
-		vpr_info("query %d: \"%s\" mod:%s\n", i, query, modname ?: "*");
+		vpr_info("query %d: %s%s \"%s\"\n", i,
+			 modname ? "module " : "", modname ?: "", query);
 
 		rc = ddebug_exec_query(query, modname);
 		if (rc < 0) {
@@ -1160,11 +1153,10 @@ static int ddebug_add_module(struct _ddebug_info *di, const char *modname)
 {
 	struct ddebug_table *dt;
 
-	v3pr_info("add-module: %s.%d sites\n", modname, di->num_descs);
-	if (!di->num_descs) {
-		v3pr_info(" skip %s\n", modname);
+	if (!di->num_descs)
 		return 0;
-	}
+
+	v3pr_info("add-module: %s %d sites\n", modname, di->num_descs);
 
 	dt = kzalloc_obj(*dt);
 	if (dt == NULL) {
