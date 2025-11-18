296147da288b5766f5c47bd0a8f23b5d3ed49ee3
Jim Cromie <jim.cromie@gmail.com>
2025-11-18 13:18:33 -0700

    dyndbg: check DYNAMIC_DEBUG_CLASSMAP_DEFINE args at compile-time
    Add __DYNAMIC_DEBUG_CLASSMAP_CHECK to implement the following
    arg-checks at compile-time:
    
    	0 <= _base < 63
    	class_names is not empty
    	class_names[0] is a string
    	(class_names.length + _base) < 63
    
    These compile-time checks will prevent several simple misuses, and
    will issue obvious errors if violated.
    
    4 such examples are added to test_dynamic_debug_submod.ko, and will
    fail compilation if -DDD_MACRO_ARGCHECK is added to cflags.  This
    wouldn't be a useful CONFIG_ item, since it breaks the build.
    
    Signed-off-by: Jim Cromie <jim.cromie@gmail.com>

--- lib/test_dynamic_debug.c
+++ lib/test_dynamic_debug.c
@@ -133,7 +133,7 @@ DYNAMIC_DEBUG_CLASSMAP_PARAM(level_num, p);
  * Enable with -Dflag on compile to test overlapping class-id range
  * detection.  This should warn on modprobes.
  */
-DYNDBG_CLASSMAP_DEFINE(classid_range_conflict, 0, D2_CORE + 1, "D3_CORE");
+DYNAMIC_DEBUG_CLASSMAP_DEFINE(classid_range_conflict, 0, D2_CORE + 1, "D3_CORE");
 #endif
 
 #else /* TEST_DYNAMIC_DEBUG_SUBMOD */
@@ -146,7 +146,18 @@ DYNDBG_CLASSMAP_DEFINE(classid_range_conflict, 0, D2_CORE + 1, "D3_CORE");
 DYNAMIC_DEBUG_CLASSMAP_USE(map_disjoint_bits);
 DYNAMIC_DEBUG_CLASSMAP_USE(map_level_num);
 
+#if defined(DD_MACRO_ARGCHECK)
+/*
+ * Exersize compile-time arg-checks in DYNAMIC_DEBUG_CLASSMAP_DEFINE.
+ * These will break compilation.
+ */
+DYNAMIC_DEBUG_CLASSMAP_DEFINE(fail_base_neg, 0, -1, "NEGATIVE_BASE_ARG");
+DYNAMIC_DEBUG_CLASSMAP_DEFINE(fail_base_big, 0, 100, "TOOBIG_BASE_ARG");
+DYNAMIC_DEBUG_CLASSMAP_DEFINE(fail_str_type, 0, 0, 1 /* not a string */);
+DYNAMIC_DEBUG_CLASSMAP_DEFINE(fail_emptyclass, 0, 0 /* ,empty */);
 #endif
+
+#endif /* TEST_DYNAMIC_DEBUG_SUBMOD */
 
 /* stand-in for all pr_debug etc */
 #define prdbg(SYM) __pr_debug_cls(SYM, #SYM " msg\n")
