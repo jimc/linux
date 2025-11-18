4bc9edbd916d32a02c1108024fa81de2bcc65385
Jim Cromie <jim.cromie@gmail.com>
2025-11-18 13:18:17 -0700

    dyndbg: Upgrade class param storage to u64 for 64-bit classmaps
    Currently, `struct ddebug_class_param` uses `unsigned long` pointers
    to store the state of `bits` and `lvl`. On 32-bit architectures, this
    limits the bit-vector to 32 bits, which truncates 64-bit classmaps
    (such as the one needed by `__drm_debug`).
    
    To guarantee support for 64-bit debug categories across all
    architectures, upgrade the internal storage types in `struct
    _ddebug_class_param` to `u64`. This inherently makes both union
    members the same size while safely avoiding truncation.
    
    This includes:
    - Changing union members `bits` and `lvl` to `u64 *`.
    - Using `kstrtou64()` for sysfs input parsing.
    - Replacing array-based `test_bit()` operations with safe `u64` scalar
      bitwise logic (`!!(val & (1ULL << bi))`).
    - adjusting lib/test_dynamic_debug.c too
    
    Not yet done:
    - Updating `drm_print`'s `__drm_debug` to `u64` and configuring the
      associated module parameter as `ullong`.
    - Fixing an unused variable warning in `__drm_printfn_dbg`.
    
    Signed-off-by: Jim Cromie <jim.cromie@gmail.com>
    ---
    v2:
    
    patch was "make bits & lvl same size"
    but that size was unsigned long, only 32 bits on i386 etc
    use u64 for all bits, and %llu %llx

--- lib/test_dynamic_debug.c
+++ lib/test_dynamic_debug.c
@@ -40,7 +40,7 @@ module_param_cb(do_prints, &param_ops_do_prints, NULL, 0600);
  * - tie together sysname, mapname, bitsname, flagsname
  */
 #define DD_SYS_WRAP(_model, _flags)					\
-	static unsigned long bits_##_model;				\
+	static u64 bits_##_model;					\
 	static struct ddebug_class_param _flags##_model = {		\
 		.bits = &bits_##_model,					\
 		.flags = #_flags,					\
