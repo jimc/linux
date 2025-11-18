88b93a365fe586cd534abfa28fbbd1e2fda006a9
Jim Cromie <jim.cromie@gmail.com>
2025-11-18 13:18:25 -0700

    dyndbg,module: make proper substructs in _ddebug_info
    recompose struct _ddebug_info, inserting proper sub-structs.
    
    The struct _ddebug_info has 2 pairs of _vec, num##_vec fields, for
    descs and classes respectively.  for_subvec() makes walking these
    vectors less cumbersome, now lets move those field pairs into their
    own "vec" structs: _ddebug_descs & _ddebug_class_maps, and re-compose
    struct _ddebug_info to contain them cleanly.  This also lets us rid
    for_subvec() of its num##_vec paste-up.
    
    Also recompose struct ddebug_table to contain a _ddebug_info.  This
    reinforces its use as a cursor into relevant data for a builtin
    module, and access to the full _ddebug state for modules.
    
    NOTES:
    
    Invariant: These vectors ref a contiguous subrange of __section memory
    in builtin/DATA or in loadable modules via mod->dyndbg_info; with
    guaranteed life-time for us.
    
    Fixup names: Normalize all struct names to "struct _ddebug_*"
    eliminating the minor/stupid variations created in classmaps-v1.
    Also normalize the __section names to "__dyndbg_*".
    
    struct module contains a _ddebug_info field and module/main.c sets it
    up, so that gets adjusted rather obviously.
    
    Signed-off-by: Jim Cromie <jim.cromie@gmail.com>

--- include/asm-generic/vmlinux.lds.h
+++ include/asm-generic/vmlinux.lds.h
@@ -385,8 +385,8 @@
 	*(__tracepoints)						\
 	/* implement dynamic printk debug */				\
 	. = ALIGN(8);							\
-	BOUNDED_SECTION_BY(__dyndbg, ___dyndbg)				\
-	BOUNDED_SECTION_BY(__dyndbg_classes, ___dyndbg_classes)		\
+	BOUNDED_SECTION_BY(__dyndbg_descriptors, ___dyndbg_descs)	\
+	BOUNDED_SECTION_BY(__dyndbg_class_maps, ___dyndbg_class_maps)	\
 	CODETAG_SECTIONS()						\
 	LIKELY_PROFILE()		       				\
 	BRANCH_PROFILE()						\
