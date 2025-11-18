7e68a7796d48a0f871f8d1fe0a8989654102cc1e
Jim Cromie <jim.cromie@gmail.com>
2025-11-18 13:18:32 -0700

    dyndbg: detect class_id reservation conflicts
    If a module _DEFINEs 2 or more classmaps, it must devise them to share
    the per-module 0..62 class-id space; ie their respective base,+length
    reservations cannot overlap.
    
    To detect conflicts at modprobe, add ddebug_class_range_overlap(),
    call it from ddebug_add_module(), and WARN and return -EINVAL when
    they're detected.
    
    This insures that class_id -> classname lookup has just 1 answer, so
    the 1st-found search in find-class-name works properly.
    
    test_dynamic_debug.c:
    
    If built with -DFORCE_CLASSID_CONFLICT, the test-modules invoke 2
    conflicting DYNAMIC_DEBUG_CLASSMAP_DEFINE() declarations, into parent
    and the _submod.  These conflict with one of the good ones in the
    parent (D2_CORE..etc), causing the modprobe(s) to WARN and fail.
    
    Signed-off-by: Jim Cromie <jim.cromie@gmail.com>
    Reviewed-by: Louis Chauvet <louis.chauvet@bootlin.com>
    ---
    v2: RvB after SoB
    
    old-v9 - fix WARN() by adding new 1st arg 1.
    old-v12 - drop maybe_unused on range-overlap fn

--- lib/dynamic_debug.c
+++ lib/dynamic_debug.c
@@ -1099,10 +1099,15 @@ static void *ddebug_proc_next(struct seq_file *m, void *p, loff_t *pos)
 	return dp;
 }
 
-static bool ddebug_class_in_range(const int class_id, const struct _ddebug_class_map *map)
+static bool ddebug_class_map_in_range(const int class_id, const struct _ddebug_class_map *map)
 {
 	return (class_id >= map->base &&
 		class_id < map->base + map->length);
+}
+
+static bool ddebug_class_user_in_range(const int class_id, const struct _ddebug_class_user *user)
+{
+	return ddebug_class_map_in_range(class_id - user->offset, user->map);
 }
 
 static const char *ddebug_class_name(struct _ddebug_info *di, struct _ddebug *dp)
@@ -1112,11 +1117,11 @@ static const char *ddebug_class_name(struct _ddebug_info *di, struct _ddebug *dp
 	int i;
 
 	for_subvec(i, map, di, maps)
-		if (ddebug_class_in_range(dp->class_id, map))
+		if (ddebug_class_map_in_range(dp->class_id, map))
 			return map->class_names[dp->class_id - map->base];
 
 	for_subvec(i, cli, di, users)
-		if (ddebug_class_in_range(dp->class_id, cli->map))
+		if (ddebug_class_user_in_range(dp->class_id, cli))
 			return cli->map->class_names[dp->class_id - cli->map->base - cli->offset];
 
 	return NULL;
@@ -1309,6 +1314,20 @@ static void ddebug_apply_class_users(const struct _ddebug_info *di)
 	__di->_vec.len = __nc;						\
 })
 
+static int ddebug_class_range_overlap(struct _ddebug_class_map *cm, u64 *reserved_ids)
+{
+	u64 range = (((1ULL << cm->length) - 1) << cm->base);
+
+	if (range & *reserved_ids) {
+		pr_err("[%d..%d] on %s conflicts with %llx\n", cm->base,
+		       cm->base + cm->length - 1, cm->class_names[0],
+		       *reserved_ids);
+		return -EINVAL;
+	}
+	*reserved_ids |= range;
+	return 0;
+}
+
 /*
  * Allocate a new ddebug_table for the given module
  * and add it to the global list.
@@ -1318,6 +1337,7 @@ static int ddebug_add_module(struct _ddebug_info *di)
 	struct ddebug_table *dt;
 	struct _ddebug_class_map *cm;
 	struct _ddebug_class_user *cli;
+	u64 reserved_ids;
 	int i;
 
 	if (!di->descs.len)
@@ -1349,18 +1369,27 @@ static int ddebug_add_module(struct _ddebug_info *di)
 	dd_set_module_subrange(i, cm, &dt->info, maps);
 	dd_set_module_subrange(i, cli, &dt->info, users);
 
-	if (dt->info.maps.len)
-		ddebug_apply_class_maps(&dt->info);
-	if (dt->info.users.len)
-		ddebug_apply_class_users(&dt->info);
+	/* insure 2+ classmaps share the per-module 0..62 class_id space */
+	for_subvec(i, cm, &dt->info, maps)
+		if (ddebug_class_range_overlap(cm, &reserved_ids))
+			goto cleanup;
 
 	mutex_lock(&ddebug_lock);
 	list_add_tail(&dt->link, &ddebug_tables);
 	mutex_unlock(&ddebug_lock);
 
+	if (dt->info.maps.len)
+		ddebug_apply_class_maps(&dt->info);
+	if (dt->info.users.len)
+		ddebug_apply_class_users(&dt->info);
+
 	vpr_info("%3u debug prints in module %s\n",
 		 dt->info.descs.len, dt->info.mod_name);
 	return 0;
+cleanup:
+	WARN_ONCE(1, "dyndbg multi-classmap conflict in %s\n", di->mod_name);
+	kfree(dt);
+	return -EINVAL;
 }
 
 /* helper for ddebug_dyndbg_(boot|module)_param_cb */
