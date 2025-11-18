4d3fdd22e2f55035d93f44cd0df0f389b3d41ed5
Jim Cromie <jim.cromie@gmail.com>
2025-11-18 13:18:37 -0700

    dyndbg: split multi-query strings with @
    Since
    commit 85f7f6c0edb8 ("dynamic_debug: process multiple debug-queries on a line")
    
    Multi-query commands have been allowed:
    
      modprobe drm dyndbg="class DRM_UT_CORE +p; class DRM_UT_KMS +p"
      modprobe drm dyndbg=<<EOX
         class DRM_UT_CORE +p
         class DRM_UT_KMS +p
      EOX
    
    More recently, the need for quoting was avoided by treating a comma
    like a space/token-terminator:
    
      modprobe drm dyndbg=class,DRM_UT_CORE,+p\;class,DRM_UT_KMS,+p
    
    That works, but it needs the escaped semicolon, which is a shell
    special-char (one of the bash control operators), so it is brittle
    when passed in/down/around scripts.
    
    So this patch adds '@' to the existing ';' and '\n' multi-command
    separators, which is more shell-friendly, so you can more fully avoid
    quoting and escaping hassles.
    
    Update selftests script, adding a multi-query split on @
    
    Signed-off-by: Jim Cromie <jim.cromie@gmail.com>
    ---
    v10:
    . replace strpbrk() with quote-aware ddebug_find_delimiter() to ignore '@' inside quotes.
    
    v9:
    . update FT_multi_query command in dyndbg_selftest.sh to actively test @ delimiter.
    v5: avoid +t flag in content tests - pid is not predictable.
        remove Reviewed-by: <louis.chauvet@bootlin.com> - too many changes
    
    v2:
    
    replace '%' with '@' as multi-query splitter, as it is:
     - not a sshell special cahr
     - allows matching on format strings with format specifiers

--- tools/testing/selftests/dynamic_debug/dyndbg_selftest.sh
+++ tools/testing/selftests/dynamic_debug/dyndbg_selftest.sh
@@ -138,7 +138,6 @@ function ddcmd_load () {
     local val="$4"
 
     # 1. Setup the control filters (using positional ddcmd range-check)
-    echo  "$query" "$range"
     ddcmd "$query" "$range"
 
     # 2. Execute the workload and capture syslog prints
@@ -332,9 +331,20 @@ function FT_basic_queries {
     ddcmd "file $f +l"  "$f"
     ddcmd "file $f -m"  "$f"
     ddcmd "file $f =_"  "$f"
+}
 
-    # multi-query commands split on ; on a single line
+function FT_multi_query {
+    v_echo "${GREEN}# MULTI_QUERY_TESTS ${NC}"
+    if [ $LACK_DD_BUILTIN -eq 1 ]; then
+	echo "SKIP - test requires dynamic_debug built into kernel"
+	return
+    fi
+    local f='kernel/params.c'
+    ddcmd =_ # zero everything
+
+    # multi-query commands on a single line, split on ;/@ respectively
     ddcmd "file $f +mf ; file $f func parse_args +sl" "$f"
+    ddcmd "file $f -f @ file $f func parse_args -l"   "$f"
 
     # verify multi-cmd input, newline separated, with embedded comments
     ddcmd =_ # reset before multiline query to capture full transition
@@ -511,7 +521,7 @@ function FT_test_classes {
     verify_control_slice '\[test_dynamic_debug\]'
 
     # 2. Verify state transition and live-printing end-to-end via ddcmd_load!
-    ddcmd_load "class D2_CORE +pmf;class D2_KMS +pls;class D2_ATOMIC +pml" \
+    ddcmd_load "class,D2_CORE,+pmf;class,D2_KMS,+pls;class,D2_ATOMIC,+pml" \
         '\[test_dynamic_debug\]' \
         "/sys/module/test_dynamic_debug/parameters/do_classes" "1"
 
@@ -609,6 +619,7 @@ builtin_tests=(
     FT_path_module_queries
     FT_hyphen_underscore
     FT_comma_terminators
+    FT_multi_query
 )
 
 # Modular Feature Tests (Require CONFIG_MODULES=y and test_dynamic_debug*.ko available)
@@ -631,8 +642,8 @@ modular_tests=(
 #
 # ==============================================================================
 function GOLDEN_RECORDS {
-    cat << 'EOF' | {
-#K= 26fd1565f209b383d9f2fbf0b54cd5fc FT_grammar_errs.1
+    cat << 'EO_DYNDBG_GOLDEN_REFS' | {
+#K= 95b249c7ec2cbd5a436f96060b2bee37 FT_grammar_errs.1
 #K= 200c01632c52a63f6d186da1c6460740 FT_grammar_errs.2
 #K= 7d7141900ce6e32f15c99202309c63a4 FT_grammar_errs.3
 #K= 1bb798a5831d0119789d424ef6cb55c4 FT_grammar_errs.4
@@ -652,7 +663,7 @@ function GOLDEN_RECORDS {
 #K= cb8288d607b0c5282125852f3ab05107 FT_grammar_errs.18
 #K= b371c6ba52503d037dbc43da788af8be FT_grammar_errs.19
 #K= 98e2bd3e4f3da58536496a38ec3e6238 FT_grammar_errs.20
-#K= 02b210b2646aceb68f3d6d2876a3d57a FT_grammar_errs.21
+#K= c4e8b7261811e9fdf0c80fa9e374a29e FT_grammar_errs.21
 #K= 51030cd01ed601a68dd48356902954b1 FT_grammar_errs.22
 #K= af8818de076407d4e7f4e384ba110ac5 FT_grammar_errs.23
 #K= 023ba04ffced833b4776fb4fcaae7755 FT_grammar_errs.24
@@ -672,7 +683,7 @@ function GOLDEN_RECORDS {
 #K= 130bf64b92a5e34d1c4cce98899084e1 FT_grammar_errs.38
 #K= 9681a05658f1a50e3434aa991b5ce992 FT_grammar_errs.39
 #K= 38506317363a188fbb7a417ff50457a9 FT_grammar_errs.40
-#K= 02b210b2646aceb68f3d6d2876a3d57a FT_grammar_errs.41
+#K= c4e8b7261811e9fdf0c80fa9e374a29e FT_grammar_errs.41
 #K= 8c08a8b5cacbf885d022360ed856e7a1 FT_grammar_errs.42
 #K= f0f4d894a49bd2c1856cf4f013f83e54 FT_grammar_errs.43
 #K= 25b92d52cb299fa8d84c5584e490bd97 FT_grammar_errs.44
@@ -696,14 +707,15 @@ function GOLDEN_RECORDS {
 #K= d8eb8f226860aa558fb8a98097f05be9 FT_basic_queries.2
 #K= 6a86bb9209a3a0492bc6c2d29f4d5e52 FT_basic_queries.3
 #K= 90804574a5336971465f92d6cc3aa7fb FT_basic_queries.4
-#K= f2b4f24fece9c55f5a5d28323c2019f8 FT_basic_queries.5
-#K= 8c2dd1164fbcefb721345ce62a864a37 FT_basic_queries.6
-#K= 4542e1e5e7eadcbe8f90a9c934635618 FT_basic_queries.7
 #K= 78ad5b168d9db27931dfe7dac93143bf FT_comma_terminators.1
 #K= 4a8d6e0468d14659030ef523c6e5a184 FT_comma_terminators.2
 #K= 3c445fb23d701041e920a2a6d2b022c7 FT_comma_terminators.3
 #K= 68b329da9893e34099c7d8ad5cb9c940 FT_comma_terminators.4
 #K= c745b8f58c0557edca58d5a224a09190 FT_comma_terminators.5
+#K= f2b4f24fece9c55f5a5d28323c2019f8 FT_multi_query.1
+#K= cce9c41c3ac297e910c3ee3b10852cb3 FT_multi_query.2
+#K= 8c2dd1164fbcefb721345ce62a864a37 FT_multi_query.3
+#K= 4542e1e5e7eadcbe8f90a9c934635618 FT_multi_query.4
 #K= 8f18ea82c09460434b6e9e4cd12543e8 FT_test_classes.1
 #K= a15ec4843acd721fbdfddc0b512c8032 FT_test_classes.2
 #K= 20d4545f9753e677e72e3adf52527fd3 FT_test_classes.3
@@ -748,7 +760,7 @@ function GOLDEN_RECORDS {
 #K= 6a320774e2b535ceb89d6cdde6f5d0fb FT_modprobe_w_param.34
 #K= 7f961a7d3facd89bdd4b9d8e7b5541e9 FT_modprobe_w_param.35
 #K= 6a320774e2b535ceb89d6cdde6f5d0fb FT_modprobe_w_param.36
-EOF
+EO_DYNDBG_GOLDEN_REFS
         # Read the K-recs and skip those for tests that can't run
         while read -r line; do
             # Filter built-in if needed
