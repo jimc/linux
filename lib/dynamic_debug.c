a81abab4c185233fb149733ce95ae689b90d207b
Jim Cromie <jim.cromie@gmail.com>
2026-03-09 00:44:34 -0600

    x86/alternative.c: sort text-pokes before flushing the queue
    Until now, x86 can queue jump_label text-pokes as long as the
    poke-addr is monotonically increasing, but flushes the queue when a
    new poke-addr is less than the previous.
    
    Dynamic-debug now uses the queued static-key API, but the advantage is
    limited; we see a ~2x reduction in IPIs.  Although the pr_debug
    descriptors are ordered, the patch-address in them are not; about 1/2
    violate the ordering constraint.
    
    So this patch drops that requirement, and sorts the text-pokes by
    their address before applying them.  Doing so lets us fill the queue
    before sorting then flushing, giving a dramatic ~125x reduction in
    IPIs over the traditional single IPI per pr_debug.
    
    Other arches don't need a queue, and so have nothing to sort.
    
       #> dd_ipis
       [   23.100381] dyndbg: query 0: "module !virtio* +p " mod:*
       [   23.103432] dyndbg: query 1: "-p" mod:*
       Delta-CAL (IPI): 242
    
    Signed-off-by: Jim Cromie <jim.cromie@gmail.com>

--- lib/dynamic_debug.c
+++ lib/dynamic_debug.c
@@ -394,6 +394,7 @@ static int ddebug_change(const struct ddebug_query *query, struct flag_settings 
 	}
 	static_branch_apply_queued();
 	mutex_unlock(&ddebug_lock);
+	v2pr_info("applied %d queued updates to sites in total\n", nfound);
 
 	return nfound;
 }
