a40347d6eae964cb57a9df7d0026f103b8620b9f
Jim Cromie <jim.cromie@gmail.com>
2026-03-17 15:28:37 -0600

    vmlinux.lds.h: refactor BOUNDED_SECTION_* macros into bounded_sections.lds.h
    Move BOUNDED_SECTION_* macros to a new helper file:
    include/asm-generic/bounded_sections.lds.h and include it back into
    vmlinux.lds.h.  This allows its reuse later to fix a failure to keep
    dyndbg sections in some circumstances.
    
    NOTES:
    
    These macros are only for use in vmlinux.lds.h, where the _start &
    _end symbols are needed.  Modules keep sections separate in ELF
    sections, with their boundaries known, so the _start and _end are not
    useful, and may confuse tools not expecting them.
    
    This patch ignores a checkpatch warning, because new file is covered
    by "GENERIC INCLUDE/ASM HEADER FILES" in MAINTAINERS
    
    CC: Arnd Bergmann <arnd@arndb.de>
    CC: linux-arch@vger.kernel.org
    Signed-off-by: Jim Cromie <jim.cromie@gmail.com>
    ---
    v3: move include to top

---
+++ include/asm-generic/bounded_sections.lds.h
@@ -0,0 +1,36 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+
+#ifndef _ASM_GENERIC_BOUNDED_SECTIONS_H
+#define _ASM_GENERIC_BOUNDED_SECTIONS_H
+
+#define BOUNDED_SECTION_PRE_LABEL(_sec_, _label_, _BEGIN_, _END_)	\
+	_BEGIN_##_label_ = .;						\
+	KEEP(*(_sec_))							\
+	_END_##_label_ = .;
+
+#define BOUNDED_SECTION_POST_LABEL(_sec_, _label_, _BEGIN_, _END_)	\
+	_label_##_BEGIN_ = .;						\
+	KEEP(*(_sec_))							\
+	_label_##_END_ = .;
+
+#define BOUNDED_SECTION_BY(_sec_, _label_)				\
+	BOUNDED_SECTION_PRE_LABEL(_sec_, _label_, __start, __stop)
+
+#define BOUNDED_SECTION(_sec)	 BOUNDED_SECTION_BY(_sec, _sec)
+
+#define HEADERED_SECTION_PRE_LABEL(_sec_, _label_, _BEGIN_, _END_, _HDR_) \
+	_HDR_##_label_	= .;						\
+	KEEP(*(.gnu.linkonce.##_sec_))					\
+	BOUNDED_SECTION_PRE_LABEL(_sec_, _label_, _BEGIN_, _END_)
+
+#define HEADERED_SECTION_POST_LABEL(_sec_, _label_, _BEGIN_, _END_, _HDR_) \
+	_label_##_HDR_ = .;						\
+	KEEP(*(.gnu.linkonce.##_sec_))					\
+	BOUNDED_SECTION_POST_LABEL(_sec_, _label_, _BEGIN_, _END_)
+
+#define HEADERED_SECTION_BY(_sec_, _label_)				\
+	HEADERED_SECTION_PRE_LABEL(_sec_, _label_, __start, __stop)
+
+#define HEADERED_SECTION(_sec)	 HEADERED_SECTION_BY(_sec, _sec)
+
+#endif /* _ASM_GENERIC_BOUNDED_SECTIONS_H */
