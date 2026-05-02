6c84120aa26975b8b2df42ce8b842378f0690fdd
Jim Cromie <jim.cromie@gmail.com>
2026-05-02 08:00:02 -0600

    vmlinux.lds.h: Fix ALIGN(8) omission causing NULL ptr on i386
    Almost all uses of the BOUNDED_SECTION macros are ALIGN(8), either
    explicitly, or by being below an aligned section containing x*8 byte
    objects.  The noteworthy exception is BOUNDED_SECTION(__dyndbg), which
    immediately follows BOUNDED_SECTION(__dyndbg_classes).
    
    On i386, struct _ddebug_classmap is 28 bytes, so without an explicit
    ALIGN(8) in the macro, the following __dyndbg section gets misaligned,
    causing a NULL ptr deref in dynamic_debug_init().
    
    So fix this with an explicit ALIGN(8) in the existing BOUNDED_SECTION
    macros, and introduce _ALIGNED variants to handle the cases with an
    explicit . = ALIGN(x)
    
    Also add explicit alignments for: EXCEPTION_TABLE, ORC_UNWIND_TABLE,
    TRACEDATA, INIT_SETUP, and NOTES.
    
    update BOUNDED_SECTION uses inside . = ALIGN(x) stanzas to use
    _ALIGNED variants, but keep the outer ALIGNs so the symbols between
    them are not "re-aligned".
    
    In particular, scripts/sorttable.c does not tolerate sloppy padding.
    
    At the top of ORC_UNWIND_TABLE, add . = ALIGN(4) to match the struct
    orc_header __align() call in the code:
    
    commit b9f174c811e3 ("x86/unwind/orc: Add ELF section with ORC version identifier")
    
    Suggested-by: Louis Chauvet <louis.chauvet@bootlin.com>  # _ALIGNED variants.
    Link: https://lore.kernel.org/lkml/177402491426.6181.12855763650074831089.b4-review@b4/
    Signed-off-by: Jim Cromie <jim.cromie@gmail.com>
    ---
    v3:
    
    sashiko complained about NOTES and .BTF_ids.
    gemini asserts that NOTES are natively 4-byte aligned, add comment repeating it.
    .BTF_ids doesnt use BOUNDED_BY, since start/end isnt needed;
    sashiko evidently got confused by immediately preceding usage.
    
    v2:
    
    sashiko picked up 2 cases, added to the explicit list above
    https://sashiko.dev/#/patchset/20260515-asm-generic-1-v3-0-680b273666d4%40gmail.com

--- include/asm-generic/bounded_sections.lds.h
+++ include/asm-generic/bounded_sections.lds.h
@@ -3,19 +3,30 @@
 #ifndef _ASM_GENERIC_BOUNDED_SECTIONS_H
 #define _ASM_GENERIC_BOUNDED_SECTIONS_H
 
-#define BOUNDED_SECTION_PRE_LABEL(_sec_, _label_, _BEGIN_, _END_)	\
+#define BOUNDED_SECTION_PRE_LABEL_ALIGNED(_sec_, _label_, _BEGIN_, _END_, _ALIGNED_) \
+	. = ALIGN(_ALIGNED_);						\
 	_BEGIN_##_label_ = .;						\
 	KEEP(*(_sec_))							\
 	_END_##_label_ = .;
 
-#define BOUNDED_SECTION_POST_LABEL(_sec_, _label_, _BEGIN_, _END_)	\
+#define BOUNDED_SECTION_PRE_LABEL(_sec_, _label_, _BEGIN_, _END_)	\
+	BOUNDED_SECTION_PRE_LABEL_ALIGNED(_sec_, _label_, _BEGIN_, _END_, 8)
+
+#define BOUNDED_SECTION_POST_LABEL_ALIGNED(_sec_, _label_, _BEGIN_, _END_, _ALIGNED_) \
+	. = ALIGN(_ALIGNED_);						\
 	_label_##_BEGIN_ = .;						\
 	KEEP(*(_sec_))							\
 	_label_##_END_ = .;
 
+#define BOUNDED_SECTION_POST_LABEL(_sec_, _label_, _BEGIN_, _END_)	\
+	BOUNDED_SECTION_POST_LABEL_ALIGNED(_sec_, _label_, _BEGIN_, _END_, 8)
+
 #define BOUNDED_SECTION_BY(_sec_, _label_)				\
 	BOUNDED_SECTION_PRE_LABEL(_sec_, _label_, __start, __stop)
 
-#define BOUNDED_SECTION(_sec)	 BOUNDED_SECTION_BY(_sec, _sec)
+#define BOUNDED_SECTION_BY_ALIGNED(_sec_, _label_, _ALIGNED_)		\
+	BOUNDED_SECTION_PRE_LABEL_ALIGNED(_sec_, _label_, __start, __stop, _ALIGNED_)
+
+#define BOUNDED_SECTION(_sec)   BOUNDED_SECTION_BY(_sec, _sec)
 
 #endif /* _ASM_GENERIC_BOUNDED_SECTIONS_H */
