42d80a3bc4214d8c2da60cf229926307ce4d78a4
Jim Cromie <jim.cromie@gmail.com>
2026-08-12 00:24:45 -0600

    include/linux/folio_arena.h: Add !CONFIG_DYNAMIC_DEBUG_CORE stubs
    When CONFIG_DYNAMIC_DEBUG_CORE is disabled, folio_arena.o is not built,
    but callers like kernel/bpf/verifier.c and kernel/bpf/syscall.c include
    the header unconditionally and reference folio_arena_{init,alloc,free}(),
    causing linker errors on defconfig builds.
    
    Wrap the real struct and declarations inside #ifdef CONFIG_DYNAMIC_DEBUG_CORE
    and provide static inline no-op stubs on the else branch, so callers build
    and link cleanly regardless of config.
    
    Signed-off-by: Jim Cromie <jim.cromie@gmail.com>

--- include/linux/folio_arena.h
+++ include/linux/folio_arena.h
@@ -7,6 +7,9 @@
 #include <linux/spinlock.h>
 #include <linux/page-flags.h>
 #include <linux/mm.h>
+#include <linux/gfp.h>
+
+#ifdef CONFIG_DYNAMIC_DEBUG_CORE
 
 struct folio_arena_chunk {
 	struct list_head link;
@@ -29,8 +32,25 @@ struct folio_arena {
 	.lock = __SPIN_LOCK_UNLOCKED((name).lock),			\
 }
 
-void folio_arena_init(struct folio_arena *fa, size_t elem_size, unsigned int order);
+void folio_arena_init(struct folio_arena *fa, size_t elem_size,
+		      unsigned int order);
 void *folio_arena_alloc(struct folio_arena *fa, gfp_t gfp);
 void folio_arena_free(struct folio_arena *fa);
+
+#else /* !CONFIG_DYNAMIC_DEBUG_CORE */
+
+struct folio_arena { };
+
+#define FOLIO_ARENA_INIT(name, _elem_size, _order) { }
+
+static inline void folio_arena_init(struct folio_arena *fa, size_t elem_size,
+				    unsigned int order) { }
+static inline void *folio_arena_alloc(struct folio_arena *fa, gfp_t gfp)
+{
+	return NULL;
+}
+static inline void folio_arena_free(struct folio_arena *fa) { }
+
+#endif /* CONFIG_DYNAMIC_DEBUG_CORE */
 
 #endif /* _LINUX_FOLIO_ARENA_H */
