1a47fbce1659186abffba5e212514ed248f3053f
Jim Cromie <jim.cromie@gmail.com>
2026-09-02 18:57:52 -0600

    lib/dynamic_debug: Walk builtin sites directly in dynamic_debug_init
    Walk di.sites.start sequentially in dynamic_debug_init() to register
    builtin module subranges without round-tripping through masked keys
    in the site tree, avoiding canonical address truncation on 64-bit kernels.
    
    Signed-off-by: Jim Cromie <jim.cromie@gmail.com>

--- lib/dynamic_debug.c
+++ lib/dynamic_debug.c
@@ -2089,20 +2089,10 @@ static int __init dynamic_debug_init_control(void)
 	return 0;
 }
 
-struct ddebug_mod_info {
-	struct list_head link;
-	const char *mod_name;
-	unsigned long start_addr;
-	unsigned long end_addr;
-};
-
 static int __init dynamic_debug_init(void)
 {
-	int i, ret = 0, mod_ct = 0;
-	void *mod_name;
+	int i = 0, ret = 0, mod_ct = 0;
 	char *cmdline;
-	LIST_HEAD(mod_list);
-	struct ddebug_mod_info *mod_info, *tmp;
 
 	struct _ddebug_info di = {
 		.descs.start = __start___dyndbg_descs,
@@ -2133,56 +2123,46 @@ static int __init dynamic_debug_init(void)
 		return 0;
 	}
 	/*
-	 * fill the 3 function, file, module trees with the values and
-	 * their intervals, and then walk the module intervals and
-	 * call add_module for each.
+	 * Walk the builtin sites and add each module's subrange.
 	 */
-	ddebug_condense_sites(&di);
+	if (di.sites.len) {
+		unsigned int count = di.sites.len;
+		struct _ddebug *range_start = di.descs.start;
+		const char *cur_mod = di.sites.start[0]._modname;
 
-	/*
-	 * under rcu-lock, gather the modules' descriptor intervals
-	 * into an atomically alloc'd list
-	 */
-	rcu_read_lock();
-	{
-		unsigned long mod_start_key = ddebug_site_tag_key(0, DD_TAG_MOD);
-		unsigned long mod_end_key = ddebug_site_tag_key(ULONG_MAX, DD_TAG_MOD);
-		MA_STATE(mas, &dd_site_map, mod_start_key, mod_end_key);
+		ddebug_condense_sites(&di);
 
-		mas_for_each(&mas, mod_name, mod_end_key) {
-			mod_info = kmalloc(sizeof(*mod_info), GFP_ATOMIC);
-			if (!mod_info) {
-				pr_warn("kmalloc failed, some modules may not be processed\n");
-				break;
+		for (i = 0; i < count; i++) {
+			const char *p_mod = di.sites.start[i]._modname;
+
+			if (p_mod != cur_mod && strcmp(p_mod, cur_mod) != 0) {
+				struct _ddebug_info mod_di = di;
+
+				mod_di.mod_name = cur_mod;
+				mod_di.descs.start = range_start;
+				mod_di.descs.len = &di.descs.start[i] - range_start;
+				mod_di.sites.len = 0;
+				ret = ddebug_add_module(&mod_di);
+				if (ret)
+					goto out_err;
+				mod_ct++;
+				range_start = &di.descs.start[i];
+				cur_mod = p_mod;
 			}
-			mod_info->mod_name = (const char *)mod_name;
-			mod_info->start_addr = mas.index & DD_KEY_ADDR_MASK;
-			mod_info->end_addr = mas.last & DD_KEY_ADDR_MASK;
-			list_add_tail(&mod_info->link, &mod_list);
 		}
-	}
-	rcu_read_unlock();
+		if (range_start < di.descs.start + count) {
+			struct _ddebug_info mod_di = di;
 
-	/*
-	 * walk the list, call ddebug_add_module for each, which may sleep
-	 */
-	list_for_each_entry_safe(mod_info, tmp, &mod_list, link) {
-		struct _ddebug_info mod_di = di;
-
-		mod_di.mod_name = mod_info->mod_name;
-		mod_di.descs.start = (struct _ddebug *)mod_info->start_addr;
-		mod_di.descs.len = (mod_info->end_addr - mod_info->start_addr) / sizeof(struct _ddebug) + 1;
-
-		ret = ddebug_add_module(&mod_di);
-		if (ret) {
-			pr_err("Failed to add module %s, error %d\n",
-			       mod_di.mod_name, ret);
-			goto out_err;
+			mod_di.mod_name = cur_mod;
+			mod_di.descs.start = range_start;
+			mod_di.descs.len = (di.descs.start + count) - range_start;
+			mod_di.sites.len = 0;
+			ret = ddebug_add_module(&mod_di);
+			if (ret)
+				goto out_err;
+			mod_ct++;
 		}
-		mod_ct++;
-		i += mod_di.descs.len;
-		list_del(&mod_info->link);
-		kfree(mod_info);
+		i = count;
 	}
 
 	ddebug_init_success = 1;
@@ -2209,11 +2189,6 @@ static int __init dynamic_debug_init(void)
 	return 0;
 
 out_err:
-	/* Clean up any remaining items in mod_list on error */
-	list_for_each_entry_safe(mod_info, tmp, &mod_list, link) {
-		list_del(&mod_info->link);
-		kfree(mod_info);
-	}
 	ddebug_remove_all_tables();
 	return ret;
 }
